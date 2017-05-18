3eda11a5ebd63184a32380d361896b0d6b25fce1
Kshitij Jauhri <kshitijsjauhri@rediffmail.com>
2017-05-18 23:22:55 +0530

    Added some more solutions

---
+++ Intermediate/SortedUnion.js
@@ -0,0 +1,27 @@
+/*
+    Write a function that takes two or more arrays and returns a new array of unique values in the order of the original provided arrays.
+
+    In other words, all values present from all arrays should be included in their original order, but with no duplicates in the final array.
+
+    The unique numbers should be sorted by their original order, but the final array should not be sorted in numerical order.
+*/
+
+
+function uniteUnique(arr) {
+  
+  for(var i=0;i<arguments.length;i++)
+    {
+      for(var j=0;j<arguments[i].length;j++)
+        {
+          if(!arr.includes(arguments[i][j]))
+            {
+              arr.push(arguments[i][j]);
+            }
+        }
+    }
+  
+  
+  return arr;
+}
+
+console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]));
---
+++ Intermediate/SpinalTapCase.js
@@ -0,0 +1,5 @@
+/*
+    Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+*/
+
+
